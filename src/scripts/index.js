e1b2c8495c8254f3b0112b0019ab4ddcc0d915ff
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2023-02-26 20:55:34 +0700

    implement eslint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,14 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
-import '../scripts/components/restaurant-list';
+import './components/restaurant-list';
 import DataSource from '../DATA.json';
 
 const restaurantListElement = document.querySelector('restaurant-list');
 const menu = document.querySelector('#hamburgerButton');
 const drawer = document.querySelector('#navigationDrawer');
 
-menu.addEventListener('click', function (event) {
+menu.addEventListener('click', (event) => {
   drawer.classList.toggle('open');
   event.stopPropagation();
 });
