a0766b5aa1623113c6231a1486cb73e5ed7d40f9
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2023-02-26 16:07:38 +0700

    new nav draft

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,17 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 import '../scripts/components/restaurant-list';
 import DataSource from '../DATA.json';
 
 const restaurantListElement = document.querySelector('restaurant-list');
+const menu = document.querySelector('#hamburgerButton');
+const drawer = document.querySelector('#navigationDrawer');
+
+menu.addEventListener('click', function (event) {
+  drawer.classList.toggle('open');
+  event.stopPropagation();
+});
 
 const getRestaurantList = () => {
   restaurantListElement.items = DataSource.restaurants;
