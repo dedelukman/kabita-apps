70a24816efc73789859d4506a6242fd7448fd172
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2023-02-27 08:38:47 +0700

    draft detail

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,10 +1,16 @@
 import DicodingRestaurantDbSource from '../../data/dicodingRestaurantDbSource';
 import UrlParser from '../../routes/url-parser';
+import { createRestoDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
-        <h2>Detail Page</h2>
+    <section class="content">
+  <div class="restaurant_list">
+    <h1 class="restaurant_list__label">Explore Restaurant</h1>
+    <div id="restaurants" class="restaurants"></div>
+  </div>
+</section>
       `;
   },
 
@@ -12,6 +18,8 @@ const Detail = {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const resto = await DicodingRestaurantDbSource.detail(url.id);
     console.log(resto);
+    const restoContainer = document.querySelector('#restaurants');
+    restoContainer.innerHTML = createRestoDetailTemplate(resto.restaurant);
   },
 };
 
