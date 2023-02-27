11b62b08dc521b39949128026ef93b5e0fe305af
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2023-02-28 06:30:31 +0700

    service + cache

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,6 +3,7 @@ const CONFIG = {
   BASE_IMAGE_SMALL_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   BASE_IMAGE_MEDIUM_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_IMAGE_LARGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
-import './components/restaurant-list';
 import App from './views/app';
+import swRegister from './utils/sw-register';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -16,4 +16,5 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
   app.renderPage();
+  swRegister();
 });
